e83b30f6f9079c1336905a2749877bf3650f6ca0
nurseiit <nurseiiit.tursunkulov@2peaches.de>
2025-01-01 13:10:57 +0100

    Add package sources

--- Package.swift
+++ Package.swift
@@ -5,17 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "2PeachesOnboarding2",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "2PeachesOnboarding2",
             targets: ["2PeachesOnboarding2"]),
     ],
+    dependencies: [
+        // Adding the CHIPageControl dependency from GitHub
+        .package(url: "https://github.com/ChiliLabs/CHIPageControl.git", from: "0.1.3")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "2PeachesOnboarding2"),
-
+            name: "2PeachesOnboarding2",
+            dependencies: ["CHIPageControl"]
+        ),
+        
     ]
 )
